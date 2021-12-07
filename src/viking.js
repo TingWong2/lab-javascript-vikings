a8b37ddb232a2b74f9cbe112094ce0b03e42b7eb
Ting Wong <ting_wong2@yahoo.fr>
2021-12-07 22:09:52 +0100

    iteration 1 2 3

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
 
-// Viking
-class Viking {}
+  
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  
+  attack() {
+    return this.strength;
+  } ; 
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }; 
+  
+}
+
+
+//  Viking 
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength); 
+    this.name = name; 
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0 ) {
+      return (`${this.name} has received ${damage} points of damage`)
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }; 
+    
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return  `A Saxon has received ${damage} points of damage`
+    } else {
+      return "A Saxon has died in combat"
+    }
+  }
+};
 
 // War
-class War {}
+class War {
+
+  constructor( ) {
+  this.vikingArmy = [];
+  this.saxonArmy = []; 
+  }
+
+  addViking(1) {
+    this.addViking + this.vikingArmy
+  }
+  addSaxon(1) {
+    this.addSaxon + this.addSaxon
+  }
+  vikingAttack()
+  saxonAttack()
+  showStatus()
+  
+
+}
 
 
 
